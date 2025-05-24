a01b8b51bafda4918d089b0c45eb08e494f9b65d
박신혜 <p6612sh@naver.com>
2025-05-25 02:56:58 +0900

    알림 전체 목록 페이지 추가

--- polling-app-client/src/app/App.js
+++ polling-app-client/src/app/App.js
@@ -18,9 +18,7 @@ import AppHeader from '../common/AppHeader';
 import NotFound from '../common/NotFound';
 import LoadingIndicator from '../common/LoadingIndicator';
 import PrivateRoute from '../common/PrivateRoute';
-
-import { Layout, notification } from 'antd';
-const { Content } = Layout;
+import NewGroupOage from '../frontend/new-group-page/NewGroupPage';
 
 class App extends Component {
   constructor(props) {
@@ -109,9 +107,10 @@ class App extends Component {
                 <Route path="/users/:username" 
                   render={(props) => <Profile isAuthenticated={this.state.isAuthenticated} currentUser={this.state.currentUser} {...props}  />}>
                 </Route>
+	    <Route path="/groups/new" component={NewGroupPage} /
                 <PrivateRoute authenticated={this.state.isAuthenticated} path="/poll/new" component={NewPoll} handleLogout={this.handleLogout}></PrivateRoute>
                 <Route component={NotFound}></Route>
-              </Switch>
+	    </Switch>
             </div>
           </Content>
         </Layout>
